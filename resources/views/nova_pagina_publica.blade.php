6881d9e98bcbc377365fbba3c3c9be8264f5cddd
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-11-06 17:11:46 -0300

    -- Mudando o componente da página para poder exibir a mensagem de usuário não autenticado quando a rota for acessada antes de efetuar o login

--- resources/views/nova_pagina_publica.blade.php
+++ resources/views/nova_pagina_publica.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-guest-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             Nova Página Pública
@@ -20,4 +20,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-guest-layout>
